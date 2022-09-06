6f1640bbc16ce71aa5bc0cec878cc0425cbb4bb3
liushuo6666 <lshpnn@163.com>
2022-09-06 18:34:05 +0800

    创建及注册子应用

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--=cz8vhx9!w2og+=_z%%v9wir#l*b+vxt#b!1=8)nppbf&5oh=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -97,13 +98,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/shangahi"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
